f775623160826122e92f9b1e3bef150d1b52a32b
tclark <tclark@qmetric.co.uk>
2013-02-20 14:15:56 +0000

    Extract methods and paths

--- src/main/groovy/donkey/DocGenerator.groovy
+++ src/main/groovy/donkey/DocGenerator.groovy
@@ -10,7 +10,7 @@ import javax.lang.model.element.ElementKind
 import javax.lang.model.element.TypeElement
 import javax.tools.Diagnostic
 import javax.tools.StandardLocation
-import javax.ws.rs.Path
+import javax.ws.rs.*
 
 @SupportedAnnotationTypes("javax.ws.rs.Path") @SupportedSourceVersion(SourceVersion.RELEASE_6)
 class DocGenerator extends AbstractProcessor
@@ -20,7 +20,7 @@ class DocGenerator extends AbstractProcessor
         roundEnvironment.getElementsAnnotatedWith(Path).each {
             if (it.kind == ElementKind.CLASS)
             {
-                message("Found: " + it.simpleName + " (" + processingEnv.elementUtils.getDocComment(it) + ")")
+                message("Found: " + it.simpleName)
                 store(it, "rest-doc-" + it.simpleName.toString() + ".html", markup(it))
             }
         }
@@ -33,15 +33,25 @@ class DocGenerator extends AbstractProcessor
         def html = new groovy.xml.MarkupBuilder(w)
 
         def intro = processingEnv.elementUtils.getDocComment(element)
+        def docs = documentation(element)
 
         html.html {
             head {
                 title 'REST API for ' + element.simpleName.toString()
             }
-            body    {
-                if(intro != null)   {
+            body {
+                if (intro != null)
+                {
                     div {
                         mkp.yieldUnescaped intro
+                    }
+                }
+                table {
+                    docs.each { d ->
+                        tr {
+                            td(d.method)
+                            td(d.path)
+                        }
                     }
                 }
             }
@@ -50,14 +60,96 @@ class DocGenerator extends AbstractProcessor
         return w.toString()
     }
 
+    def documentation(final Element element)
+    {
+        def basePath = element.getAnnotation(Path).value()
+        def result = new ArrayList<Doc>()
+
+        element.enclosedElements.each {
+            if (it.kind == ElementKind.METHOD)
+            {
+                if (shouldDocument(it))
+                {
+                    result.add(new Doc(method(it), path(basePath, it)))
+                }
+            }
+        }
+
+        return result
+    }
+
+    def path(final String basePath, final Element element)  {
+        if (element.getAnnotation(Path) != null)    {
+            return element.getAnnotation(Path).value()
+        }
+
+        return basePath
+    }
+
+    def shouldDocument(final Element element)
+    {
+        if (element.getAnnotation(GET))
+        {
+            return true
+        }
+        if (element.getAnnotation(POST))
+        {
+            return true
+        }
+        if (element.getAnnotation(PUT))
+        {
+            return true
+        }
+        if (element.getAnnotation(DELETE))
+        {
+            return true
+        }
+
+        return false
+    }
+
+    def method(final Element element)
+    {
+        if (element.getAnnotation(GET))
+        {
+            return "GET"
+        }
+        if (element.getAnnotation(POST))
+        {
+            return "POST"
+        }
+        if (element.getAnnotation(PUT))
+        {
+            return "PUT"
+        }
+        if (element.getAnnotation(DELETE))
+        {
+            return "DELETE"
+        }
+
+        return "UNKNOWN"
+    }
+
     def store(final Element element, final String filename, final String content)
     {
-        def file = processingEnv.getFiler().createResource(StandardLocation.SOURCE_OUTPUT, "", filename, element)
-        file.openWriter().withWriter { w -> w.write(content) }
+        processingEnv.getFiler().createResource(StandardLocation.SOURCE_OUTPUT, "", filename, element).openWriter().withWriter { w -> w.write(content) }
     }
 
     def message(final String message)
     {
         processingEnv.messager.printMessage(Diagnostic.Kind.NOTE, message)
+    }
+}
+
+class Doc
+{
+    final String method
+    final String path
+    String description
+
+    Doc(final String method, final String path)
+    {
+        this.method = method
+        this.path = path
     }
 }
